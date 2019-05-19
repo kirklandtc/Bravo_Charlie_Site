58b8189cebcb65e8806ea2d5619e07cdefc7a043
Thomas Kirkland <thomasckirkland@gmail.com>
2019-05-19 13:35:06 -0600

    new content

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ $('body').scrollspy({ target: '#navbar-example' })
 $('[data-spy="scroll"]').each(function () {
   var $spy = $(this).scrollspy('refresh')
 })
+
+$(document).ready(function () {
+$('#dtBasicExample').DataTable();
+$('.dataTables_length').addClass('bs-select');
+});
